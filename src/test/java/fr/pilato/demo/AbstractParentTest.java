765239c2150b0ebec25773c5f662cdbe4409799e
David Pilato <david@pilato.fr>
2024-08-05 18:10:31 +0200

    Add 1st demo

---
+++ src/test/java/fr/pilato/demo/AbstractParentTest.java
@@ -0,0 +1,31 @@
+/*
+   Copyright 2024 David Pilato
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package fr.pilato.demo;
+
+import org.tinylog.Logger;
+
+abstract public class AbstractParentTest {
+    private final static String API_KEY_ENV_NAME = "OPENAI_API_KEY";
+
+    static String getOpenAiApiKey() {
+        String apiKey = System.getenv(API_KEY_ENV_NAME);
+        if (apiKey == null || apiKey.isEmpty()) {
+            Logger.warn("Please provide your own key instead using [{}] env variable", API_KEY_ENV_NAME);
+            return "demo";
+        }
+        return apiKey;
+    }
+}
