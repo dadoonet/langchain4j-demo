87c74cbf88e5e3b5ea86890bf7dbacb1810f5ec2
David Pilato <david@pilato.fr>
2024-08-21 15:54:18 +0200

    Add Ollama

--- README.md
+++ README.md
@@ -14,3 +14,18 @@ None yet
 
 The first demo is only making a simple connection to OpenAI to ask it 
 a simple question.
+
+See [Step1AiChatTest.java](src/test/java/fr/pilato/demo/Step1AiChatTest.java) class.
+
+### Demo 2: with more context
+
+We can give some context before asking questions to the LLM.
+
+See [Step2AssistantTest.java](src/test/java/fr/pilato/demo/Step2AssistantTest.java) class.
+
+### Demo 3: running the LLM locally with Ollama
+
+We can use the great [Ollama project](https://ollama.com/). It helps to run a LLM locally
+on your machine.
+
+See [Step3OllamaTest.java](src/test/java/fr/pilato/demo/Step3OllamaTest.java) class.
---
+++ src/test/java/fr/pilato/demo/Step3OllamaTest.java
@@ -0,0 +1,64 @@
+/*
+   Copyright 2024 David Pilato
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package fr.pilato.demo;
+
+import dev.langchain4j.model.ollama.OllamaChatModel;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.service.SystemMessage;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.ollama.OllamaContainer;
+import org.testcontainers.utility.DockerImageName;
+import org.tinylog.Logger;
+
+public class Step3OllamaTest extends AbstractParentTest {
+
+    static String MODEL_NAME = "mistral";
+    static String DOCKER_IMAGE_NAME = "langchain4j/ollama-" + MODEL_NAME + ":latest";
+
+    static OllamaContainer ollama = new OllamaContainer(
+            DockerImageName.parse(DOCKER_IMAGE_NAME).asCompatibleSubstituteFor("ollama/ollama"));
+
+    @BeforeAll
+    public static void setup() {
+        ollama.start();
+    }
+
+    @AfterAll
+    public static void teardown() {
+        ollama.stop();
+    }
+
+    interface Assistant {
+        @SystemMessage("Please answer in a funny way.")
+        String chat(String userMessage);
+    }
+
+    @Test
+    public void testAiChat() {
+        OllamaChatModel model = OllamaChatModel.builder()
+                .baseUrl(ollama.getEndpoint())
+                .modelName(MODEL_NAME)
+                .temperature(0.8)
+                .build();
+
+        Assistant assistant = AiServices.create(Assistant.class, model);
+
+        String answer = assistant.chat("Who is Thomas Pesquet?");
+        Logger.info("Answer is: {}", answer);
+    }
+}
